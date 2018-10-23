6fcc3b721fa49f807dcc0f4d1b5a79f5472c843c
abstructs <andrewclarke540@gmail.com>
2018-10-23 15:49:06 -0400

    implement O(3n) version of isPerm

--- andrews_algorithms/src/arraysAndStrings/CheckPermutation.java
+++ andrews_algorithms/src/arraysAndStrings/CheckPermutation.java
@@ -1,11 +1,34 @@
 package arraysAndStrings;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CheckPermutation {
 	
 	public static void main(String args[]) {
-		System.out.println(isPermSlow("abc", "ab"));
+		System.out.println(isPermFast("abc", "bac"));
+		
+	}
+	
+	public static Boolean isPermFast(String str1, String str2) {
+		return getOccurs(str1).equals(getOccurs(str2));
+	}
+	
+	public static HashMap<Character, Integer> getOccurs(String str) {
+		HashMap<Character, Integer> occurs = new HashMap<Character, Integer>();
+		for(int i = 0; i < str.length(); i++) {
+			Character key = str.charAt(i);
+			Integer value = occurs.get(key);
+			
+			if(occurs.containsKey(key)) {
+				occurs.put(key, value + 1);
+			} 
+			else {
+				occurs.put(key, 0);
+			}
+		}
+		
+		return occurs;
 	}
 	
 	public static Boolean isPermSlow(String str1, String str2) {
